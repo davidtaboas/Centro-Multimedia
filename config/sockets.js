9be152f1aa280d7f37921d32c73144933497146f
David Táboas <david.taboas.glez@gmail.com>
2014-06-20 05:22:26 +0200

    Socket.io por proxy

--- config/sockets.js
+++ config/sockets.js
@@ -20,7 +20,7 @@ module.exports = function (server, config) {
     var iomonitor = require('socket.io').listen(runningPortMonitor);
     var apimessage = require('socket.io').listen(runningPortApi);
 
-    //iousers.set('transports', ['websocket', 'xhr-polling']);
+    iousers.set('transports', ['websocket', 'xhr-polling']);
     //iousers.set('log level', 2);
     iomonitor.set('log level', 2);
 
