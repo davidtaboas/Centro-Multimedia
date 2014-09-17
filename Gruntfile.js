7fd721403a824177113538db1fe01636ca9f6aeb
David Táboas <david.taboas.glez@gmail.com>
2014-09-17 17:41:08 +0200

    Cambio grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -118,7 +118,7 @@ module.exports = function (grunt) {
         tasks: ['develop', 'delayed-livereload']
       },
       jade: {
-        files: ['app/views/**/*.jade'],
+        files: ['app/views/**/*.jade', 'modules/**/*.jade'],
         options: { livereload: reloadPort }
       },
       compass: {
